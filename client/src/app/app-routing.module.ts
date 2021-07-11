67552dfadd4ff715e04963d84ae164aeaf5e4097
Sameera911 <sameera.n9@gmail.com>
2021-07-12 00:40:24 +0530

    End of section 7

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,26 +1,33 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
-//import {AuthGuard} from './_guards/auth.guard';
+import {AuthGuard} from './_guards/auth.guard';
+
 const routes: Routes = [
   {path: '', component:HomeComponent },
-  // {
-  //   path:'',
-  //   runGuardsAndResolvers:'always',
-  //   canActivate:[AuthGuard],
-  //   children:[
-      {path:'members',component:MemberListComponent},
+  {
+    path:'',
+    runGuardsAndResolvers:'always',
+    canActivate:[AuthGuard],
+    children:[
+      {path:'members',component:MemberListComponent,  canActivate:[AuthGuard]},
       {path:'members/:1d',component:MemberDetailComponent},
       {path:'lists',component:ListsComponent},
       {path:'messages',component:MessagesComponent},
-      {path:'**',component:HomeComponent,pathMatch:'full'}
-  //   ]
-  // }
-  
+      
+    ]
+  },
+  {path:'errors',component:TestErrorsComponent,pathMatch:'full'},
+  {path:'not-found',component:NotFoundComponent,pathMatch:'full'},
+  {path:'server-error',component:ServerErrorComponent,pathMatch:'full'},
+  {path:'**',component:NotFoundComponent,pathMatch:'full'},
 ];
 
 @NgModule({
