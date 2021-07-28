1ae6c2a18d9c41740681053e2007b9087a3be720
Sameera911 <sameera.n9@gmail.com>
2021-07-28 12:38:34 +0530

    End

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -12,10 +12,7 @@ namespace API.Data
     public class DataContext : IdentityDbContext<AppUser, AppRole , int, 
     IdentityUserClaim<int>, AppUserRole, IdentityUserLogin<int>, 
     IdentityRoleClaim<int>, IdentityUserToken<int>>
-    // IdentityDbContext<AppUser, AppRole, int,
-    //     IdentityUserClaim<int>, AppUserRole, IdentityUserLogin<int>,
-    //     IdentityRoleClaim<int>, IdentityUserToken<int>>
-    {
+        {
         public DataContext(DbContextOptions options) : base(options)
         {
         }
@@ -40,24 +37,6 @@ namespace API.Data
                 .WithOne(u => u.Role)
                 .HasForeignKey(ur => ur.RoleId)
                 .IsRequired();
-
-            // builder.Entity<Group>()
-            //     .HasMany(x => x.Connections)
-            //     .WithOne()
-            //     .OnDelete(DeleteBehavior.Cascade);
-
-            // builder.Entity<AppUser>()
-            //     .HasMany(ur => ur.UserRoles)
-            //     .WithOne(u => u.User)
-            //     .HasForeignKey(ur => ur.UserId)
-            //     .IsRequired();
-
-            // builder.Entity<AppRole>()
-            //     .HasMany(ur => ur.UserRoles)
-            //     .WithOne(u => u.Role)
-            //     .HasForeignKey(ur => ur.RoleId)
-            //     .IsRequired();
-
 
             builder.Entity<UserLike>()
                 .HasKey(k => new { k.SourceUserId, k.LikedUserId });
